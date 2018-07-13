b5aab6ed1b8e4ad73e66b11f5dd8893c980c85a5
Simon Zelazny <zelazny@mailbox.org>
2018-07-12 17:00:02 -0700

    Found size of message that triggers hang

--- testing/correctness/apps/leaky_pipeline/leaker.py
+++ testing/correctness/apps/leaky_pipeline/leaker.py
@@ -3,6 +3,20 @@ from copy import deepcopy
 import wallaroo
 import os
 
+# Set LIST_LENGTH to a number between 1 and 100
+# to find where the limit is.
+
+LIST_LENGTH=80
+
+#LIST_LENGTH=80 : EXPECTING 15936 : WORKS
+#LIST_LENGTH=82 : EXPECTING 16350 : WORKS
+### 2^^14 == 16384 #### this seems to be the breaking point
+#LIST_LENGTH=83 : EXPECTING 16557 : HANGS
+#LIST_LENGTH=85 : EXPECTING 16971 : HANG
+#LIST_LENGTH=87 : EXPECTING 17385 : HANG
+#LIST_LENGTH=88 : EXPECTING 17592 : HANG
+
+
 def application_setup(args):
     in_host, in_port = wallaroo.tcp_parse_input_addrs(args)[0]
     out_host, out_port = wallaroo.tcp_parse_output_addrs(args)[0]
@@ -21,12 +35,11 @@ def buffer_items(item, buffer):
     ret = buffer.update(item)
     if ret:
         print "stateful ran on %s"%(os.getpid())
-    return (ret, False)
+    return (ret, True)
 
 @wallaroo.computation(name="parallel")
 def p(x):
-    print "stateless ran on %s with %s items"%(
-        os.getpid(),len(x))
+    print "stateless ran on %s"%(os.getpid())
     return x
 
 @wallaroo.decoder(header_length=4, length_fmt=">I")
@@ -37,17 +50,18 @@ def decoder(line):
 @wallaroo.encoder
 def encoder(results):
     print ("encoder ran on %s"%(os.getpid()))
-    return "NODE %s GOT %s results\n"%(os.getpid(), len(results))
+    return "NODE %s GOT %s results\n"%(os.getpid(), results)
 
 class Buffer():
     def __init__(self):
         self._items = []
+    # def update(self, thing):
+    #     return thing
     def update(self, thing):
         self._items.append(thing)
         if len(self._items) > 100:
             print "returning items"
-#            popped = self._items[0:1] # change me to 53
-            popped = self._items[0:90]
+            popped = self._items[0:LIST_LENGTH]
             self._items = []
             return popped
         else:
