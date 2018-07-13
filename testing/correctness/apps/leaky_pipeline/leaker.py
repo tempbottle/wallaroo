8e4e9ed20489a12b9791b471d23186b114a57b93
Simon Zelazny <zelazny@mailbox.org>
2018-07-12 17:53:35 -0700

    remove irrelevant state step

--- testing/correctness/apps/leaky_pipeline/leaker.py
+++ testing/correctness/apps/leaky_pipeline/leaker.py
@@ -11,23 +11,9 @@ def application_setup(args):
 
     ab = wallaroo.ApplicationBuilder("Hanging message send")
     ab.new_pipeline("Hangy pipeline", tcp_source)
-    ab.to_stateful(stateful, Dud, "s")
     ab.to_parallel(stateless)
     ab.to_sink(tcp_sink)
     return ab.build()
-
-@wallaroo.state_computation(name="s")
-def stateful(_item, _buffer):
-    # 0 = 291, 1 = 295, 2 = 299, 100 = 691, 1000 = 4291
-    # Formula: stringlen*4 + 291
-    # 4023 = 16383
-    # 4024 = 16387 # HANGS
-    ret = '\x01' * 4024
-    return (ret, False)
-
-class Dud():
-    def __init__(self):
-        True
 
 @wallaroo.computation(name="stateless")
 def stateless(x):
@@ -36,7 +22,11 @@ def stateless(x):
 
 @wallaroo.decoder(header_length=4, length_fmt=">I")
 def decoder(line):
-    return("x")
+    # 0 = 291, 1 = 295, 2 = 299, 100 = 691, 1000 = 4291
+    # Formula: stringlen*4 + 291
+    # 4023 = 16383
+    # 4024 = 16387 # HANGS
+    return('\x01' * 4023)
 
 @wallaroo.encoder
 def encoder(results):
